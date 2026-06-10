d4048a843f88dd4559ceb5af84407cdf896ad9e7
Luiz Henrique dos Santos <lhsantos198@gmail.com>
2026-06-09 21:39:16 -0300

    Atualizando mapa cpm filtro

--- App/mapa.py
+++ App/mapa.py
@@ -24,30 +24,49 @@ df_api = pd.DataFrame(lista_api)
 # CSV + API
 df = df.merge(df_api, on="uf", how="left")
 
-mapa = folium.Map(
+# mapa com filtro por região
+mapa_regioes = folium.Map(
     location=[-14.2350, -51.9253],
     zoom_start=4
 )
 
-for linha in df.itertuples():
+# aqui faremos uma camada para cada região
+for regiao in df["regiao"].unique():
 
-    texto = f"""
-    <b>{linha.capital}</b><br>
-    Estado: {linha.estado}<br>
-    UF: {linha.uf}<br>
-    Região: {linha.regiao}<br>
-    Código IBGE do Estado: {linha.codigo_ibge_estado}<br>
-    Região pela API: {linha.regiao_api}<br>
-    Latitude: {linha.latitude}<br>
-    Longitude: {linha.longitude}
-    """
+    # usamos o FeatureGroup para conseguir criar um grupo de marcadores
+    grupo_regiao = folium.FeatureGroup(name=regiao)
 
-    folium.Marker(
-        location=[linha.latitude, linha.longitude],
-        popup=folium.Popup(texto, max_width=300),
-        tooltip=linha.capital
-    ).add_to(mapa)
+    # filtramos as capitais daquela região
+    dados_regiao = df[df["regiao"] == regiao]
 
-mapa.save("mapa_capitais_brasil.html")
+    # colocamos as capitais de suas respectivas regiões no grupo indicado a elas
+    for linha in dados_regiao.itertuples():
 
-mapa
+        texto_popup = f"""
+        <b>{linha.capital}</b><br>
+        Estado: {linha.estado}<br>
+        UF: {linha.uf}<br>
+        Região: {linha.regiao}<br>
+        Código IBGE do Estado: {linha.codigo_ibge_estado}<br>
+        Região pela API: {linha.regiao_api}<br>
+        Latitude: {linha.latitude}<br>
+        Longitude: {linha.longitude}
+        """
+
+        folium.Marker(
+            location=[linha.latitude, linha.longitude],
+            popup=folium.Popup(texto_popup, max_width=300),
+            tooltip=linha.capital
+        ).add_to(grupo_regiao)
+
+    # adicionamos o grupo no mapa
+    grupo_regiao.add_to(mapa_regioes)
+
+# aqui temos o controle para mostrar ou nao determinada região no mapa
+folium.LayerControl(collapsed=False).add_to(mapa_regioes)
+
+mapa_regioes.save("mapa_capitais_brasil_regioes.html")
+
+mapa_regioes
+
+# docker run --rm -v "${PWD}\App:/app" pc-python (para rodar o mapa no docker)
